3c8d70c8511430e00d470e0d4be6e76a4d35fe00
AlexandreCalmonJr <87951095+AlexandreCalmonJr@users.noreply.github.com>
2026-08-11 21:23:31 -0300

    Add files via upload

---
+++ JAVA-BASICO/src/main/java/org/example/Aritmeticos.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Aritmeticos {
+    static void main() {
+        //Declaração de Variaveis
+        int a = 10;
+        int b = 2;
+
+        // Operadores aritmeticos
+        int mulitplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+
+        // Exibindo resultados
+        System.out.println("Multiplicar: " + mulitplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+         
+    }
+}
